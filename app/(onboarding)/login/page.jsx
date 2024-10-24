9831cb45c506e94898a8069f164554b3c51b5483
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2024-10-25 00:38:13 +0500

    Update page.jsx

--- app/(onboarding)/login/page.jsx
+++ app/(onboarding)/login/page.jsx
@@ -20,6 +20,7 @@ const Login = () => {
         <div className="md:w-8/12 w-full bg-white flex justify-center items-center">
           <Image src={logo} className='w-2/4 p-10' alt='text' />
         </div>
+
         <div className="md:w-4/12 w-full flex p-4 flex-col justify-center bg-white ">
           <h1 className='text-2xl font-bold font-mono  text-blue-400'>Welcome to Adam's Drive</h1>
           <span className='text-gray-600 text-lg font-thin '>Sign-in</span>
@@ -28,17 +29,22 @@ const Login = () => {
           <input placeholder='Password' type='password' onChange={(e)=>setpassword(e.target.value)}  className='p-3 w-full   border-2 border-blue-300 m-4 ml-0 hover:border-blue-900 transition-all duration-200 ease-in-out rounded-md ' />
           <button className='absolute top-7   right-4'><FaEye className='text-blue-500 m-1'/></button>
           </div>
+
           <div className="Remember-forgot  flex justify-between   m-3 mt-0 ml-0">
+
             <div className="remember flex ">
             <input type='checkbox' className='accent-blue-500 mr-3 w-4 ' />
             <span>Remember me </span>
             </div>
+
             <div className="forgot flex">
             <button className='text-blue-600'> Forgot password?</button>
             </div>
             
           </div>
+          
           <button onClick={handlesubmit} className='w-full bg-blue-500 p-3 mt-2 rounded-md text-white hover:bg-blue-900  transition-all duration-200 ease-in-out'>Log In</button>
+        
         </div>
       </div>
     </div>
