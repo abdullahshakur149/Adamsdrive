fcc39b1e92116f5b82b5ca49e0e34714fd912908
Mansoor khan <107978052+mansoor369@users.noreply.github.com>
2024-10-24 18:42:43 +0500

    added alll the pages

---
+++ app/(onboarding)/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "@/app/globals.css"
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+                <body>
+                <main>{children}</main>
+                </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/(website)/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./page.css"
+const MainLanding = () => {
+  return (
+    <div>MainLanding</div>
+  )
+}
+
+export default MainLanding
