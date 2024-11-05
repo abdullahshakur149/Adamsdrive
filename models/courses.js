760764734162f37690b29c75132fd565a3e8b083
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2024-11-05 19:23:36 +0500

    courses added

--- models/courses.js
+++ models/courses.js
@@ -1,17 +1,19 @@
-import { model, models, Schema } from "mongoose"
+import { model, models, Schema } from "mongoose";
 
 const CourseSchema = new Schema({
-    courseId: { type: String },
-    courseTitle: { type: String },
-    courseCategory: { type: String },
+    courseId: { type: String, required: true },
+    courseTitle: { type: String, required: true },
+    courseCategory: { type: String, enum: ['intensive', 'hourly'], required: true },
     courseDescription: { type: String },
-    coursePrice: { type: Number },
-    bestSeller: { type: String },
-},
-    {
-        timestamps: true
-    }
-)
+    coursePrice: { type: Number, required: true },
+    duration: { type: Number, required: true }, 
+    fastTrackIncluded: { type: Boolean, default: false },
+    bestSeller: { type: Boolean, default: false },
+    fastTrackTest: { type: Boolean, default: true }
+}, {
+    timestamps: true
+});
 
+const Course = models.Course || model('Course', CourseSchema);
 
-const Course = models.Course || model('Course', CourseSchema)
+export default Course;
