a37e1ca203abd77cb69accff19f428f73efb7f82
abdullahshakur <abdullahshakur95@gmail.com>
2024-11-15 12:39:43 +0500

    new changes

--- components/LandingPage/Intro.jsx
+++ components/LandingPage/Intro.jsx
@@ -1,24 +1,38 @@
-"use client"
-import React, { useEffect } from 'react';
-import Image from 'next/image';
-import { FaCheckCircle, FaCar, FaUser, FaSync, FaClipboardList } from 'react-icons/fa';
-import intro from '@/public/img/bg.png';
-import 'aos/dist/aos.css';
-import Aos from 'aos';
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import {
+  FaCheckCircle,
+  FaCar,
+  FaUser,
+  FaSync,
+  FaClipboardList,
+} from "react-icons/fa";
+import intro from "@/public/img/bg.png";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 const IntroSection = () => {
-  useEffect(()=>{
+  useEffect(() => {
     Aos.init({
-      duration:1000,
-      once:false
-    })
-  },[])
+      duration: 1000,
+      once: false,
+    });
+  }, []);
   return (
     <div className=" w-full   ">
       {/* Intro Image and Text */}
-      <Image src={intro} className="w-full " data-aos='fade-in' alt="Driving School" />
-      <div data-aos="fade-right" data-aos-delay= "1500" className="absolute top-20   left-10 sm:top-32 sm:left-24 md:top-28 md:left-24 lg:top-32 lg:left-36 xl:top-52 xl:left-52 2xl:left-80 2xl:top-72     text-black">
-      
+      <Image
+        src={intro}
+        className="w-full "
+        data-aos="fade-in"
+        alt="Driving School"
+      />
+      <div
+        data-aos="fade-right"
+        data-aos-delay="1500"
+        className="absolute top-20   left-10 sm:top-32 sm:left-24 md:top-28 md:left-24 lg:top-32 lg:left-36 xl:top-38 xl:left-52 2xl:left-80 2xl:top-72     text-black"
+      >
         <h1 className="  text-xs  sm:text-xl md:text-xl lg:text-4xl xl:text-5xl 2xl:text-6xl  font-monaBold leading-snug">
           The truest drive <br /> comes from doing <br /> what you love.
         </h1>
@@ -53,13 +67,18 @@ const IntroSection = () => {
       </div>
 
       {/* Stats Section */}
-      <div data-aos='fade-in' className="container   bg-default   p-4 sm:p-6 md:p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center mx-auto lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 ">
+      <div
+        data-aos="fade-in"
+        className="container   bg-default   p-4 sm:p-6 md:p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center mx-auto lg:grid-cols-4 gap-4 sm:gap-6 md:gap-8 "
+      >
         <div className="stat flex">
           <div className="icon">
             <FaUser className="text-orange-500 text-2xl sm:text-3xl md:text-2xl mx-auto mb-1 mt-1 mr-3" />
           </div>
           <div className="info">
-            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">8649</h2>
+            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">
+              8649
+            </h2>
             <p className="text-xs  xl:text-base font-bold font-sans">
               Students are trained <br /> every year by our instructors
             </p>
@@ -70,7 +89,9 @@ const IntroSection = () => {
             <FaCar className="text-orange-500 text-2xl sm:text-3xl md:text-2xl mx-auto mb-1 mt-1 mr-3" />
           </div>
           <div className="info">
-            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">76</h2>
+            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">
+              76
+            </h2>
             <p className="text-xs  xl:text-base font-bold font-sans">
               Category B and C Vehicles <br /> available for our students
             </p>
@@ -81,7 +102,9 @@ const IntroSection = () => {
             <FaClipboardList className="text-orange-500 text-2xl sm:text-3xl md:text-2xl mx-auto mb-1 mt-1 mr-3" />
           </div>
           <div className="info">
-            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">99.8%</h2>
+            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">
+              99.8%
+            </h2>
             <p className="text-xs  xl:text-base font-bold font-sans">
               Our students pass <br /> the state exam the first time
             </p>
@@ -92,9 +115,11 @@ const IntroSection = () => {
             <FaSync className="text-orange-500 text-2xl sm:text-3xl md:text-2xl mx-auto mb-1 mt-1 mr-3" />
           </div>
           <div className="info  md:mb-16 lg:mb-0">
-            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">3472</h2>
+            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-monaBold">
+              3472
+            </h2>
             <p className="text-xs  xl:text-base font-bold font-sans">
-              Our students drive so many<br></br>  kilometers every day
+              Our students drive so many<br></br> kilometers every day
             </p>
           </div>
         </div>
