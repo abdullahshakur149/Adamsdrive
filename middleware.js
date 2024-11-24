69476f7da83056c38d9334b5962de743461db66e
abdullahshakur <abdullahshakur95@gmail.com>
2024-11-24 16:41:21 +0500

    middleware

---
+++ middleware.js
@@ -0,0 +1,17 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+    function middleware(req) {
+    },
+    {
+        callbacks: {
+            authorized: ({ token }) => {
+                return token?.role === "admin";
+            },
+        },
+    }
+);
+
+export const config = {
+    matcher: ["/dashboard/:path*"],
+};
