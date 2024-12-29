955f935e472d9c09a16f507f340edfc0f8fd19ac
abdullahshakur <abdullahshakur95@gmail.com>
2024-12-29 17:35:12 +0500

    PostalCode created

--- app/api/PostalCode/route.js
+++ app/api/PostalCode/route.js
@@ -35,13 +35,37 @@ import PostalCode from '@/models/PostalCode';
 //     }
 // }
 
-export async function GET() {
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import PostalCode from "@/models/PostalCode";
+
+export async function POST(req) {
     try {
+        // Connect to the database
         await connectDB();
-        const postalCodes = await PostalCode.find()
-        return NextResponse.json({ status: 200, postalCodes })
+
+        const { postalCode } = await req.json();
+
+        // Check  the postal code  in the database
+        const postalCodeData = await PostalCode.findOne({ postalCode });
+
+        if (!postalCodeData) {
+            return NextResponse.json({
+                status: 404,
+                message: "Postal code not found. Please check and try again."
+            });
+        }
+
+        return NextResponse.json({
+            status: 200,
+            message: "Postal code found. Redirecting...",
+            data: postalCodeData
+        });
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({ status: 500, message: 'Error fetching postal codes' })
+        console.log(error);
+        return NextResponse.json({
+            status: 500,
+            message: "Error processing the request. Please try again later."
+        });
     }
 }
