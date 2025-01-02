75e429aadee04190330af88286a598b793736659
abdullahshakur <abdullahshakur95@gmail.com>
2025-01-02 17:09:18 +0500

    worked on postal

--- app/api/PostalCode/route.js
+++ app/api/PostalCode/route.js
@@ -1,40 +1,3 @@
-// import { NextResponse } from 'next/server';
-// import connectDB from '@/lib/db';
-// import PostalCode from '@/models/PostalCode';
-
-// export async function POST(req) {
-//     // Connect to the database
-//     await connectDB();
-
-//     try {
-//         // Parse request body (incoming JSON)
-//         const { postalCode, location, hourlyRates } = await req.json();
-
-//         // Check if the postal code already exists
-//         const existingPostalCode = await PostalCode.findOne({ postalCode });
-//         if (existingPostalCode) {
-//             return NextResponse.json({ message: 'Postal code already exists.' }, { status: 400 });
-//         }
-
-//         // Create new postal code entry
-//         const newPostalCode = new PostalCode({
-//             postalCode,
-//             location,
-//             hourlyRates,
-//         });
-
-//         // Save the postal code to the database
-//         await newPostalCode.save();
-
-//         // Return success response
-//         return NextResponse.json({ message: 'Postal code added successfully!', data: newPostalCode }, { status: 201 });
-
-//     } catch (error) {
-//         console.error('Error adding postal code:', error);
-//         return NextResponse.json({ message: 'An error occurred while adding postal code.' }, { status: 500 });
-//     }
-// }
-
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/db";
 import PostalCode from "@/models/PostalCode";
@@ -46,8 +9,10 @@ export async function POST(req) {
 
         const { postalCode } = await req.json();
 
-        // Check  the postal code  in the database
-        const postalCodeData = await PostalCode.findOne({ postalCode });
+        // Check the postal code in the database (case-insensitive)
+        const postalCodeData = await PostalCode.findOne({
+            postalCode: { $regex: `^${postalCode.slice(0, 3)}$`, $options: "i" },
+        });
 
         if (!postalCodeData) {
             return NextResponse.json({
